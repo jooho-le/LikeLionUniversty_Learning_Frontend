790e1a6e4500672bba083466194ba22b7a9d49de
jooho-le <wngh0606@naver.com>
2026-05-03 02:08:50 +0900

    함수 1

---
+++ BasicStructure_site/JS_Beginner/functions/functions.js
@@ -0,0 +1,18 @@
+// 함수
+// 작업의 묶음 - 코드를 반복해서 작성하는 비효율을 줄임 
+
+// function multiply(x, y) {
+//     return x * y;
+// }
+
+// console.log(multiply(10, 5));
+
+// var result = multiply(20, 10);
+// console.log(result);
+
+// js만의 방법
+var multiply = function (x, y) {
+    return x * y;
+};
+
+console.log(multiply(10, 5));
