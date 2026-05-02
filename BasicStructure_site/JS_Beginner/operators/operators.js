4c809fa2df6e2aca763dd25dcdc624455863387d
jooho-le <wngh0606@naver.com>
2026-05-02 22:06:30 +0900

    연산자 2

--- BasicStructure_site/JS_Beginner/operators/operators.js
+++ BasicStructure_site/JS_Beginner/operators/operators.js
@@ -33,15 +33,47 @@
 // console.log(d %= 3);
 
 // 3. 비교 연산자 
-console.log(2 == 2);
-console.log(2 == "2"); // 값이 같으면 true 반환 , 자바스크립 엔진이 타입 변환을 시도 - 암묵적 형변환
-console.log(2 === "2"); // 값과 타입이 모두 같아야 true 반환 - 엄격한 비교 연산자
+// console.log(2 == 2);
+// console.log(2 == "2"); // 값이 같으면 true 반환 , 자바스크립 엔진이 타입 변환을 시도 - 암묵적 형변환
+// console.log(2 === "2"); // 값과 타입이 모두 같아야 true 반환 - 엄격한 비교 연산자
 
-console.log(2 != 2);
-console.log(2 != "2"); // 값이 다르면 true 반환 
-console.log(2 !== "2"); // 값과 타입이 모두 다르면 true 반환
+// console.log(2 != 2);
+// console.log(2 != "2"); // 값이 다르면 true 반환 
+// console.log(2 !== "2"); // 값과 타입이 모두 다르면 true 반환
 
-console.log(4 > 2);
-console.log(4 < 2);
-console.log(4 >= 2);
-console.log(4 <= 2);
+// console.log(4 > 2);
+// console.log(4 < 2);
+// console.log(4 >= 2);
+// console.log(4 <= 2);
+
+// 4. 논리 연산자 
+// // 논리합 : 피연산자 중 하나라도 참이면 참 
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+// console.log(2>3 || 5 == 5); // 앞에서부터 평가해서 반환 
+
+// // 논리곱 : 피연산자 모두가 참이어야 참 
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+// console.log(2>3 || 5 == 5); // 앞에서부터 평가해서 반환
+
+// // 부정 : 피연산자의 boolean값을 부정 
+// console.log(!true);
+// console.log(!false);
+// console.log(!(5>4));
+
+// 5. typeof 연산자 
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof Symbol());
+
+console.log(typeof null); // object 반환 - 자바스크립트의 버그
+// 보통 typeof보다 == 을 사용 
+
+
