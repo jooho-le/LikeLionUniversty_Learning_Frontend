e0d8dc4a6be389be2a6e05eb8db47e079340f31b
jooho-le <wngh0606@naver.com>
2026-05-02 22:18:03 +0900

    문자열 다루기 1

---
+++ BasicStructure_site/JS_Beginner/string/string.js
@@ -0,0 +1,38 @@
+// 문자열 선언 
+// var greeting = "hello";
+// var farewell = 'bye' + 1; 
+// var myname = '이멋사';
+
+// var sentence = greeting + '\n' + myname;
+// console.log(sentence);
+
+// console.log(farewell + 2); // 문자열과 숫자 연산 -> 문자열로 변환되어서 연산됨
+
+// 이스케이프 시퀀스
+// var sentence = greeting + '\n' + myname;
+// var sentence = greeting + '\t' + myname;
+// var sentence = greeting + '\'' + myname;
+// var sentence = greeting + '\"' + myname;
+// var sentence = greeting + '\\' + myname;
+// console.log(sentence);
+
+// 템플릿 리터럴
+// 멀티라인 텍스트 작업 가능 , 이스케이스 시퀀스 안써도 가능 
+// var a = `hello`;
+// console.log(a);
+
+// var b = `bye
+// everyone`;
+// console.log(b);
+
+// var price = 100;
+// var b = '이 물건은' + price + '원';
+// console.log(b);
+
+// var c = `이 물건은 ${price}원`;
+// console.log(c);
+
+// var d = `이 물건은 ${200 + 200}원`; // 변수말고 연산도 가능 
+// console.log(d);
+
+// 문자열 함수
