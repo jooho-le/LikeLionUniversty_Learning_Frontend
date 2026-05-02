91ab31305f02a9367d5d098dbbd5f8a6e4ae63ff
jooho-le <wngh0606@naver.com>
2026-05-02 22:24:52 +0900

    문자열 다루기 2

--- BasicStructure_site/JS_Beginner/string/string.js
+++ BasicStructure_site/JS_Beginner/string/string.js
@@ -36,3 +36,25 @@
 // console.log(d);
 
 // 문자열 함수
+var abc = 'i am a lion';
+// indexOf(); // 문자열에서 특정 문자나 문자열이 처음으로 나타나는 위치 반환
+console.log(abc.indexOf('lion')); 
+console.log(abc.indexOf('i'));
+
+// slice(); // 문자열의 일부분을 추출하여 반환
+console.log(abc.slice(4));
+console.log(abc.slice(0,3)); // 0부터 2까지 추출
+
+// toUpperCase(); // 문자열을 대문자로 변환하여 반환
+// toLowerCase(); // 문자열을 소문자로 변환하여 반환
+console.log(abc.toUpperCase());
+console.log(abc.toLowerCase());
+
+// startWith(); // 문자열이 특정 문자나 문자열로 시작하는지 여부를 반환
+// endWith(); // 문자열이 특정 문자나 문자열로 끝나는지 여부를 반환
+console.log(abc.startsWith('i'));
+console.log(abc.endsWith('lion'));
+
+// includes(); // 문자열이 특정 문자나 문자열을 포함하는지 여부를 반환
+console.log(abc.includes('am'));
+console.log(abc.includes('you'));
