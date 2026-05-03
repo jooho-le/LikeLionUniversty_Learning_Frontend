69f044f3e4fd810ac80ef8f651bfd1db46bdd375
jooho-le <wngh0606@naver.com>
2026-05-03 15:37:15 +0900

    함수 3

--- BasicStructure_site/JS_Beginner/functions/functions.js
+++ BasicStructure_site/JS_Beginner/functions/functions.js
@@ -44,20 +44,42 @@
 // let word; // let은 호이스팅이 일어나지 않기 때문에 word는 ReferenceError가 된다. (let은 호이스팅이 일어나지 않는다.)
 
 // const
-const b = 2; // const는 초기화 해야함 
-console.log(b);
+// const b = 2; // const는 초기화 해야함 
+// console.log(b);
 
-b = 3; // const는 재할당이 안됨 (const는 재할당이 안된다.)
+// b = 3; // const는 재할당이 안됨 (const는 재할당이 안된다.)
 
-const PI = 3.14; // 보통 대문자로 작성해서 상수임을 명시하는 방식 많이 사용 
+// const PI = 3.14; // 보통 대문자로 작성해서 상수임을 명시하는 방식 많이 사용 
 
-// const로 선언된 변수에 할당된 객체 
-const student = {
-    grade: 1,
-    class: 2
-};
-student.grade = 2; // const로 선언된 변수에 할당된 객체의 프로퍼티는 변경이 가능하다. 
-console.log(student);
-// 객체 전체를 재할당은 불가능 
+// // const로 선언된 변수에 할당된 객체 
+// const student = {
+//     grade: 1,
+//     class: 2
+// };
+// student.grade = 2; // const로 선언된 변수에 할당된 객체의 프로퍼티는 변경이 가능하다. 
+// console.log(student);
+// // 객체 전체를 재할당은 불가능 
 
-// * var, let, const를 적절히 설계해야함 
+// // * var, let, const를 적절히 설계해야함 
+
+
+// 화살표 함수 
+// let plus = (a, b) => {
+//     return a + b;
+// };
+
+// // 간략히 표현 
+// let plus2 = (a, b) => a + b;
+
+// console.log(plus(2, 3));
+
+// let print = word => { // 매가변수가 하나인 경우 괄호 생략 가능 
+//     console.log(word);
+// }
+// print('hello');
+
+let myfunc = () => { // 파라미터 없을 떄 
+    return 1;
+}
+
+console.log(myfunc());
